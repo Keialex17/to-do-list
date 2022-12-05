6bfbd97add1d9fc5be750311b57f92baaa1b554b
Keialex17 <hernandezkeiber34@gmail.com>
2022-12-05 20:23:52 +0000

    listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,50 +1,68 @@
 import React, { useState } from "react";
 
-//create your first component
-const initialState = { label: "", done: false };
-
-const Home = () => {
-  const [tarea, setTarea] = useState(initialState);
-
-  const[lista, setLista] = useState([])
-
-  const handleChange = (event) => {
-    setTarea({
-      ...tarea,
-      [event.target.name]: event.target.value,
-    });
+const ToDo = () => {
+  const [taskList, setTaskList] = useState([]);
+  const [task, setTask] = useState("");
+  const [isShown, setIsShown] =useState(-1);
+  
+  
+  const handlertask = (event) => {
+    setTask(event.target.value);
   };
 
-  const agregarTarea= (event)=>{
-	if(event.key=="Enter"){
-		setLista([
-			...lista,
-			tarea
-		])
-	}
-	console.log(event.key)
-  }
+  const handlerKeyPress = (event) => {
+    // event.preventDefault();
 
-
+    if (event.key == "Enter" && task != "") {
+    
+        setTaskList([...taskList, task]);
+        setTask("");
+      
+    }
+  };
+  const handlerButtomDelete = (indexid) => setTaskList(taskList.filter((tarea , index)=> (index != indexid)))
+            
   return (
     <div className="container">
-      <div className="row">
-        <div className="col-12 col-md-6">
-          <form onSubmit={(event)=>{event.preventDefault()}}>
-            <input>
-              className="form-control"
+    <div className="row vh-100 colores">
+      <div className="title d-flex justify-content-center pt-2">
+        <h1>Lista de tareas</h1>
+      </div>
+      <div className="col-3"></div>
+      <div className="col-6">
+        <div className="Card" id="card">
+          <div className="form-floating mb-3">
+            <input
+              onChange={handlertask}
+              value={task}
+              onKeyDown={handlerKeyPress}
               type="text"
-              placeholder="ingresar tarea"
-              name="label"
-			  value={tarea.label}
-			  onChange={handleChange}
-			  onKeyDown={agregarTarea}
-		</input>
-        </form>
+              className="form-control  "
+              id="floatingInput"
+              placeholder="Tarea por hacer"
+            />
+            
+            <label htmlFor="floatingInput">No hay tareas, añadir tareas.</label>
+            <div className="task"></div>
+            {taskList.map((tarea, i) => {
+              return (
+              <span className="d-flex justify-content-between py-2 px-3 g-tareas text-black my-1
+              rounded-1 border border border-info" key={`s-${i}`}
+              onMouseEnter={() => {setIsShown(i)}} onMouseLeave={() => {setIsShown(-1)}} >
+                <h2 key={i}>{tarea}</h2> 
+               
+                { isShown == i &&
+                <i className="fas fa-minus-circle mt-3 ms-4 position-relative me-3 " 
+                key={`p-${i}`} onClick={() => {handlerButtomDelete(i)}}></i>}
+              </span>);
+              
+            })}
+          </div>
         </div>
       </div>
+    </div>
     </div>
   );
 };
 
-export default Home;
+export default ToDo;
